f86dd664ecbf44b6a31b3c28cec944c14008aa0c
Karol Kozakowski <github@karolkozakowski.io>
2024-01-23 21:55:22 +0100

    Describe images

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,37 +1,24 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_ecr::{config::Region, meta::PKG_VERSION, Error};
 use aws_sdk_ecr::types::Repository;
+use aws_sdk_ecr::Client;
 
-async fn show_images(
-    client: &aws_sdk_ecr::Client,
-    repository: &str,
-) -> Result<(), aws_sdk_ecr::Error> {
-    let rsp = client
-        .list_images()
-        .repository_name(repository)
-        .send()
-        .await?;
-
-    let images = rsp.image_ids();
-
-    println!("found {} images", images.len());
-
-    for image in images {
-        println!(
-            "image: {}:{}",
-            image.image_tag().unwrap(),
-            image.image_digest().unwrap()
-        );
-    }
-
-    Ok(())
-}
-
-async fn describe_repositories(client: &aws_sdk_ecr::Client) -> Result<Vec::<Repository>, aws_sdk_ecr::Error> {
+async fn describe_repositories(client: &Client) -> Result<Vec::<Repository>, aws_sdk_ecr::Error> {
     let rsp = client.describe_repositories().send().await?;
     let repositories = rsp.repositories().to_vec();
 
     Ok(repositories)
+}
+
+async fn describe_images(client: &Client, name: &str) -> Result<(), aws_sdk_ecr::Error> {
+   let rsp = client.list_images().repository_name(name).send().await?;
+   let images = rsp.image_ids();
+
+   for image in images {
+       println!("Image: {:?}", image.image_tag())
+   }
+
+   Ok(())
 }
 
 #[tokio::main]
@@ -55,6 +42,7 @@ async fn main() -> Result<(), Error> {
     let repos = describe_repositories(&client).await?;
 
     for repo in repos {
+        describe_images(&client, repo.repository_name().unwrap()).await?;
         println!("Repo: {}", repo.repository_name().unwrap_or_default());
     }
 
