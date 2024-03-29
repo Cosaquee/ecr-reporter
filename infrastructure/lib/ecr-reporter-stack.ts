58e4b53a2cf4bf8d7a859407e0c406f3b2cf3d58
Karol Kozakowski <github@karolkozakowski.io>
2024-03-29 20:53:59 +0100

    Change ECR repositories to resources from aws cdk instead of CF

--- infrastructure/lib/ecr-reporter-stack.ts
+++ infrastructure/lib/ecr-reporter-stack.ts
@@ -1,29 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
-
+import { Duration } from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 
 export class EcrReporterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new cdk.CfnResource(this, 'debian', {
-      type: 'AWS::ECR::Repository',
-      properties: {
-        RepositoryName: 'debian',
-        imageScanningConfiguration: {
-          scanOnPush: true
-        }
-      },
+    const ubuntu = new ecr.Repository(this, 'ubuntu', {
+      repositoryName: 'ubuntu',
+      imageScanOnPush: true
     });
 
-     new cdk.CfnResource(this, 'ubuntu', {
-      type: 'AWS::ECR::Repository',
-      properties: {
-        RepositoryName: 'ubuntu',
-        imageScanningConfiguration: {
-          scanOnPush: true
-        }
-      }
+    ubuntu.addLifecycleRule({ maxImageAge: Duration.days(30) });
+
+    const debian = new ecr.Repository(this, 'debian', {
+      repositoryName: 'debian',
+      imageScanOnPush: true
     });
+
+    debian.addLifecycleRule({ maxImageAge: Duration.days(30) });
   }
 }
